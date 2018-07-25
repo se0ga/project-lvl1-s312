3ca179728bf25eac5dbd68e08fd981a376b1db77
sergeiiak <sergeiiak@yandex-team.ru>
2018-07-25 10:18:25 +0100

    fix magic number

--- src/games/runBrainEven.php
+++ src/games/runBrainEven.php
@@ -5,14 +5,20 @@ namespace BrainGames\Cli;
 use function \cli\line;
 use function \cli\prompt;
 
+const NUMBER_OF_TRIES = 3;
+
+function isEven($number)
+{
+    return $number % 2 === 0;
+}
+
 function runBrainEven()
 {
-    for ($i = 0; $i < 3; $i += 1) {
+    for ($i = 0; $i < NUMBER_OF_TRIES; $i += 1) {
         $randomNumber = rand(0, 100);
-        $isEven = $randomNumber % 2 === 0;
         line("Question: %s", $randomNumber);
         $answer = prompt('Your answer: ');
-        if ($answer === 'yes' && $isEven || $answer === 'no' && !$isEven) {
+        if ($answer === 'yes' && isEven($randomNumber) || $answer === 'no' && !isEven($randomNumber)) {
             line("Correct!");
         } else {
             return false;
