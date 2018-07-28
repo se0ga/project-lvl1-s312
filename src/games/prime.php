7301a7a77b3ce649cf78f2f09068ab9b66c8e44d
sergeiiak <sergeiiak@yandex-team.ru>
2018-07-28 20:10:10 +0100

    minor fix

--- src/games/prime.php
+++ src/games/prime.php
@@ -10,6 +10,9 @@ const QUESTION_MAX_VALUE = 1000;
 
 function isPrime($number)
 {
+    if ($number < 2) {
+        return false;
+    }
     for ($i = 2; $i <= $number / 2; $i += 1) {
         if ($number % $i === 0) {
             return false;
